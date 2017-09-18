4dddb025a06f06d1695e126c6b55bab6a2892b8d
Prateek Kumar Singh <prateeksingh1590@gmail.com>
2017-09-18 12:42:06 +0530

    commit 2 Day#3

--- 16_Tuples_List.py
+++ 16_Tuples_List.py
@@ -1,7 +1,7 @@
 # to understand the difference run the below code in python, to take you to a webpage with some explaination
 
-#import webbrowser; webbrowser.open('https://stackoverflow.com/questions/626759/whats-the-difference-between-lists-and-tuples')
-
+# import webbrowser; webbrowser.open('https://stackoverflow.com/questions/626759/whats-the-difference-between-lists-and-tuples')
+ 
 # use () parenthesis to define a tuple, Tuples are immutable (Constant) data structure,
 # which are heterogeneous in comparatively faster than list
 
@@ -12,9 +12,9 @@ list = [1,2,3,4]
 print('List: ',list)
 list.append(5) #.append() to add an item at the end
 print('List after append:  ',list) 
-list.insert(2,34)
-print('List after inserting at index 2:  ',list) 
-
+list.insert(2,34) # .insert(index,value) to insert an item in a list at index value
+print('List after inserting 34 at index 2:  ',list) 
+list.remove() 
 
 print("Tuple index = 1 is ",tuple[1]) # nuber in square bracket is index = 1
 
