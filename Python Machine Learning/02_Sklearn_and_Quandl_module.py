c724c2edacd268715f97ba0460a25febbe5854e5
Prateek Kumar Singh <prateeksingh1590@gmail.com>
2017-11-03 05:20:20 +0530

    machine learnining 1-o-1

--- Python Machine Learning/02_Sklearn_and_Quandl_module.py
+++ Python Machine Learning/02_Sklearn_and_Quandl_module.py
@@ -10,24 +10,26 @@
 # python.exe -m pip install quandl
 
 import pandas as pd
-import quandl
-import math
+import quandl, math
 
 df = quandl.get('WIKI/GOOGL')
 
 # to get ytpes of values each dataframe colum holds
 # print(df.dtypes)
 
+# use a dataframe data stucture to hold features
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 df['HighLow Percent'] = (df['Adj. High'] - df['Adj. Close'])/ df['Adj. Close'] * 100.0
 df['Percent Change'] = (df['Adj. Close'] - df['Adj. Open'])/ df['Adj. Open'] * 100.0
-
 df = df[['Adj. Close', 'HighLow Percent', 'Percent Change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
+# performing a simple shift operation to forecast resultsface
 forecast_out = int(math.ceil(0.01*len(df)))
-
+#print(len(df))
+#print(forecast_out)
 df['Forecast'] = df[forecast_col].shift(-forecast_out)
+
 print(df.head(5))
