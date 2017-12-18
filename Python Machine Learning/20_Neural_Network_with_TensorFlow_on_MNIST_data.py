0a94fb59b7cd4e3f2c0eec1ab787d72df40de76d
Prateek Singh <prateeksingh1590@gmail.com>
2017-12-19 03:32:42 +0530

    Day 23 commit#3

---
+++ Python Machine Learning/20_Neural_Network_with_TensorFlow_on_MNIST_data.py
@@ -0,0 +1,128 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+'''
+input > weight > hiddenlayer1 (Activation Function) > 
+weight > hiddenlayer2 (Activation Function) >
+weight > hiddenlayer3 (Activation Function) >
+weight > output layer
+
+compare results with intended output > cost function (cross entropy)
+optimization function (optimizer) > minimize cost (adamOptimizer, SGD, RELU AdaGrad..etc)
+
+back propogation
+
+feed forward + backprop = epoch
+'''
+
+mnist = input_data.read_data_sets('/samplefiles/', one_hot=True)
+
+# number of nodes in hidden layers
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 100  # data has to be computed in realistic sized data sets
+
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+
+# define the model
+def neural_network_model(data):
+    hidden_1_layer = {
+        'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))
+    }
+
+    hidden_2_layer = {
+        'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))
+    }
+
+    hidden_3_layer = {
+        'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))
+    }
+
+    output_layer = {
+        'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+        'biases': tf.Variable(tf.random_normal([n_classes])),
+    }
+
+    # WEIGHT:
+    # =======
+    # although the ideal initial values for weights that will maximize the effectiveness and speed
+    # with which a neural network learns cannot yet be determined theoretically
+    # it is general practice to assign randomly-generated positive and negative quantities as the initial weight values. 
+    # Such a random distribution can help minimize the chances of the network becoming stuck in local minima
+    #
+    # BIAS : 
+    # ======
+    # The bias is a value that is added to our sums, before being passed through the activation function, 
+    # not to be confused with a bias node, which is just a node that is always on. 
+    # The purpose of the bias here is mainly to handle for scenarios where all neurons fired a 0 into the layer. 
+    # A bias makes it possible that a neuron still fires out of that layer.
+    
+    
+    
+    # layer1 = (data * weightsOfHiddenLayer1) + biasesOfHiddenLayer1
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+
+    # ReLU is an 'activation function' also called an rectifier
+    # a unit employing the rectifier is also called a rectified linear unit
+    l1 = tf.nn.relu(l1)
+
+    # layer2 = (data * weightsOfHiddenLayer2) + biasesOfHiddenLayer2
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    # layer3 = (data * weightsOfHiddenLayer3) + biasesOfHiddenLayer3
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+
+    return output
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+    # OLD VERSION:
+    #cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    # NEW:
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    # EPOCH:
+    # Epoch is a one complete pass of training data through a neural network, 
+    # which includes Feeding forward and back propogation
+    # larger the 'epoch' longer it takes to train
+
+    hm_epochs = 10
+    with tf.Session() as sess:
+        # OLD:
+        #sess.run(tf.initialize_all_variables())
+        # NEW:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples / batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x,y: epoch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:',
+              accuracy.eval({
+                  x: mnist.test.images,
+                  y: mnist.test.labels
+              }))
+
+
+train_neural_network(x)
---
+++ Python Machine Learning/21_Deep_Learning_Neural_Network_and_Sentiment_Analysis.py
@@ -0,0 +1,3 @@
+# python -m pip install nltk
+# import nltk
+# nltk.download()
