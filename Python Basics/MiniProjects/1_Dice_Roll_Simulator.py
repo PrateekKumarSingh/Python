23ae6b028a4ade4f0ae1de454a283886e1cbdd8f
Paginini <71335531+Paginini@users.noreply.github.com>
2020-10-02 21:27:44 +0530

    Update 1_Dice_Roll_Simulator.py

--- Python Basics/MiniProjects/1_Dice_Roll_Simulator.py
+++ Python Basics/MiniProjects/1_Dice_Roll_Simulator.py
@@ -7,3 +7,22 @@ while True:
     else:
         print('You chose to exit!')    
         sys.exit() # to exit the script
+        
+        
+# Double Dice Game---Monopoly
+
+import random
+import sys
+import randint
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+while True:
+    basic_condition = input("Would you like to play Monopoly? {YES/NO}")
+    if basic_condition == "Yes".capitalize:
+        print("Lets GO!")
+        print('''Dice 1 : {0} Dice 2 : {1}  '''.format(dice1)
+    elif basic_condition == "no".capitalize :
+    print('Bye')
+    sys.exit()
+    else:
+        print("Invalid Command! RESTART THE GAME!")
