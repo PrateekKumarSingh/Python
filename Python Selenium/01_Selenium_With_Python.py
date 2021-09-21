cae40a7ad9ecc95d44d7e465524330cca7a26ad4
Prateek Singh <prateek@ridicurious.com>
2021-09-21 16:37:24 +0530

    Update 01_Selenium_With_Python.py

--- Python Selenium/01_Selenium_With_Python.py
+++ Python Selenium/01_Selenium_With_Python.py
@@ -12,7 +12,7 @@ username.text
      
 password = driver.find_element_by_name("pass")
 password.clear()
-password.send_keys("Durg@v@ti@123")
+password.send_keys("Password@123")
 password.send_keys(Keys.RETURN) # Keys.RETURN is equal to pressing 'ENTER' button
 
 driver.get("https://www.facebook.com/groups/powershell/") # navigate to the URL using .get("URL") method
